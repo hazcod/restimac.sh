2e981bed5496d66891f6da030bd5ba3ec575a0d3
HazCod <HazCod@users.noreply.github.com>
2019-01-21 09:29:40 +0100

    Add documentation

--- restimac.sh
+++ restimac.sh
@@ -1,34 +1,44 @@
 #!/usr/bin/env bash
 
+# setup input arguments
 triggerssid="$1"
 destination="$2"
 passFile="$3"
-airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 
+# airport cli
+airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+# temporary exclusion file
 exclusionFile="/tmp/restimac.exclusion"
 
+# check input parameters
 if [ -z "$triggerssid" ] || [ -z "$destination" ] || [ ! -f "$passFile" ]; then
 	echo "Usage: $0 <ssid> <backup-destination> <pass-file>"
 	exit 1
 fi
 
+# get current WiFi SSID
 ssid=$($airport -I | grep ' SSID' | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
+# do nothing when we don't get our wanted SSID
 if [ "$ssid" != "$triggerssid" ]; then
 	exit 0
 fi
 
+# find home directory
 baseDir="/Users/$(whoami)/"
+# extract built-in TimeMachine exclusions
 /usr/libexec/PlistBuddy \
 	-c "print :UserPathsExcluded" /System/Library/CoreServices/backupd.bundle/Contents/Resources/StdExclusions.plist \
 	| sed '1d;$d' | awk '{$1=$1};1' > "$exclusionFile"
 
+# add some more exclusions
 echo "
 Downloads/
 **/.cache/
 Library/
 " >> "$exclusionFile"
 
+# start backup
 echo "Backup up $baseDir to $destination"
 /usr/local/bin/restic \
 	--quiet \
